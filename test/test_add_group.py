8a656b275abf88d4b0f9d1db64cb68eab85da877
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-02-18 00:10:36 +0300

    Создан файл-helper group, в который перенесены методы, относящиеся к группам. Произведен рефакторинг этих методов, тестов и конструктора фикстуры вследствие данного изменения

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="dsadas", header="dasdasdasd", footer="dsadsad"))
+    app.group.create(Group(name="dsadas", header="dasdasdasd", footer="dsadsad"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
