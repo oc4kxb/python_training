0a86f27f9d74b000685f20f2cb6462faeb9c4a88
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-02-18 01:40:03 +0300

    Добавлен тест для удаления первой группы в списке элементов DOM

--- fixture/group.py
+++ fixture/group.py
@@ -26,6 +26,16 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def delete_first(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # click delete group button
+        wd.find_element_by_name("delete").click()
+        # return to the groups page
+        self.return_to_groups_page()
+
     def return_to_groups_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("group page").click()
