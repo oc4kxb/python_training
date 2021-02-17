05ba2038383946acf115a39510c6f1cfb7333061
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-02-17 23:05:59 +0300

    Тесты переведены на фреймворк pytest

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,28 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 from contact import Contact
 from application import Application
-import unittest
+import pytest
 
 
-class TestAddContact(unittest.TestCase):
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def setUp(self):
-        self.app = Application()
-    
-    def test_add_contact(self):
-        self.app.login("admin", "secret")
-        self.app.create_contact(Contact(firstname="Evgeni", middlename="Valerevich", lastname="Bayrambekov", nickname="oc4kxb", company_name="MyCompany", employee_title="QA",
-                            address="region, city, street, building, flat", home_phone="44620", mobile_phone="9992002020", work_phone="9990020202",
-                            email="dadada@email.mail", homepage="mysite.com/oc4kxb", birth_day="24", birth_month="December", birth_year="1993",
-                            photo_name="fitness-gym-logo-premium-vector_144543-140.jpg"))
-        self.app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login("admin", "secret")
+    app.create_contact(Contact(firstname="Evgeni", middlename="Valerevich", lastname="Bayrambekov", nickname="oc4kxb", company_name="MyCompany", employee_title="QA",
+                        address="region, city, street, building, flat", home_phone="44620", mobile_phone="9992002020", work_phone="9990020202",
+                        email="dadada@email.mail", homepage="mysite.com/oc4kxb", birth_day="24", birth_month="December", birth_year="1993",
+                        photo_name="fitness-gym-logo-premium-vector_144543-140.jpg"))
+    app.logout()
--- test_add_group.py
+++ test_add_group.py
@@ -1,29 +1,24 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
 from group import Group
 from application import Application
-import unittest
+import pytest
 
 
-class TestAddGroup(unittest.TestCase):
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def setUp(self):
-        self.app = Application()
-    
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="dsadas", header="dasdasdasd", footer="dsadsad"))
-        self.app.logout()
 
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="dsadas", header="dasdasdasd", footer="dsadsad"))
+    app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
+
