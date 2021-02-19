cdf45a1903721a58a7f991938ccf0a7595f91302
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-02-20 00:13:36 +0300

    Произведен рефакторинг метода заполнения формы группы

--- fixture/group.py
+++ fixture/group.py
@@ -36,13 +36,13 @@ class GroupHelper:
         wd.find_element_by_link_text("group page").click()
 
     def fill_form(self, group):
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, locator, text):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        if text is not None:
+            wd.find_element_by_name(locator).click()
+            wd.find_element_by_name(locator).clear()
+            wd.find_element_by_name(locator).send_keys(text)
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,5 +3,5 @@ from model.group import Group
 
 def test_modify_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first(Group(name="EditedName", header="EditedHeader", footer="EditedFooter"))
+    app.group.modify_first(Group(name="EditedName", footer="EditedFooter"))
     app.session.logout()
