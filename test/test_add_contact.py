8f6c7d074e910b616b614dcdf180986777643496
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-02-20 14:51:31 +0300

    Переименован файл фото

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -7,5 +7,5 @@ def test_add_contact(app):
     app.contact.create(Contact(firstname="Errr", middlename="Vrrr", lastname="Brrr", nickname="oc4kxb", company_name="MyCompany", employee_title="QA",
                                address="region, city, street, building, flat", home_phone="44620", mobile_phone="9992002020", work_phone="9990020202",
                                email="dadada@email.mail", homepage="mysite.com/oc4kxb", birth_day="14", birth_month="December", birth_year="1993",
-                               photo_name="fitness-gym-logo-premium-vector_144543-140.jpg"))
+                               photo_name="avatar.jpg"))
     app.session.logout()
