f1102023c5b972ea1e2820606ec3a9eb19db3475
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-02-22 17:27:08 +0300

    Оптимизированы методы открытия страницы контактов и страницы групп

--- fixture/contact.py
+++ fixture/contact.py
@@ -43,7 +43,8 @@ class ContactHelper:
 
     def open_contacts_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("addressbook/")):
+            wd.find_element_by_link_text("home").click()
 
     def fill_form(self, contact):
         wd = self.app.wd
