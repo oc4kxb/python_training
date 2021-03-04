6d17afceb537eed1431a3ef0d07942d78a422e99
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-03-04 15:09:40 +0300

    В тесты модификации групп добавлена проверка сравнения количества групп до и после модификации. Метод count заменен при проверке на наличие групп заменен на len от get_groups_list

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,18 +2,30 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    if app.group.count() == 0:
+    if len(app.group.get_groups_list()) == 0:
         app.group.create(Group(name="modify_name_test"))
+    old_groups = app.group.get_groups_list()
     app.group.modify_first(Group(name="EditedName"))
+    new_groups = app.group.get_groups_list()
+
+    assert len(new_groups) == len(old_groups)
 
 
 def test_modify_group_header(app):
-    if app.group.count() == 0:
+    if len(app.group.get_groups_list()) == 0:
         app.group.create(Group(name="modify_header_test"))
+    old_groups = app.group.get_groups_list()
     app.group.modify_first(Group(header="EditedHeader"))
+    new_groups = app.group.get_groups_list()
+
+    assert len(new_groups) == len(old_groups)
 
 
 def test_modify_group_footer(app):
-    if app.group.count() == 0:
+    if len(app.group.get_groups_list()) == 0:
         app.group.create(Group(name="modify_footer_test"))
+    old_groups = app.group.get_groups_list()
     app.group.modify_first(Group(footer="EditedFooter"))
+    new_groups = app.group.get_groups_list()
+
+    assert len(new_groups) == len(old_groups)
