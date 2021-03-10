ca9ba54d67f89eaa3da25b584455749924294ee7
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-03-10 16:43:11 +0300

    В тестах добавления группы добавлено сравнение нового списка групп и старого с добавленим в него только что добавленной в систему группы. Реализоавана сортировка групп по ключу, а также модернизирован метод сравнения с учётом того, что нам не известен идентификатор добавленной группы в списке групп до добавления

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,16 +4,20 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_groups_list()
-    app.group.create(Group(name="dsadas", header="dasdasdasd", footer="dsadsad"))
+    group = Group(name="dsadas", header="dasdasdasd", footer="dsadsad")
+    app.group.create(group)
     new_groups = app.group.get_groups_list()
-
     assert len(new_groups) == len(old_groups) + 1
+    old_groups.append(group)
+    assert sorted(new_groups, key=Group.id_or_max) == sorted(old_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_groups_list()
-    app.group.create(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.create(group)
     new_groups = app.group.get_groups_list()
-
     assert len(new_groups) == len(old_groups) + 1
+    old_groups.append(group)
+    assert sorted(new_groups, key=Group.id_or_max) == sorted(old_groups, key=Group.id_or_max)
 
