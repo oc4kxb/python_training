1249b011f3b9b31f2d94403ed666957b6eafe47a
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-03-10 19:08:07 +0300

    В тест модификации контакта добавлена проверка модификации контакта по значению, путем замены модифицируемых данных в старом списке и сравнения отсортированных старого и нового списков

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,7 +5,11 @@ def test_modify_contact_lastname(app):
     if len(app.contact.get_contacts_list()) == 0:
         app.contact.create(Contact(lastname="modify", firstname="test"))
     old_contacts = app.contact.get_contacts_list()
-    app.contact.modify_first(Contact(lastname="EditedLastName"))
+    contact = Contact(lastname="EditedLastName")
+    contact.id = old_contacts[0].id
+    contact.firstname = old_contacts[0].firstname
+    app.contact.modify_first(contact)
     new_contacts = app.contact.get_contacts_list()
-
     assert len(new_contacts) == len(old_contacts)
+    old_contacts[0] = contact
+    assert sorted(new_contacts, key=Contact.id_or_max) == sorted(old_contacts, key=Contact.id_or_max)
