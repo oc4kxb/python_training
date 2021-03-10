77e61eccb87a1a703c7ae1e2262bab56c17fbd23
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-03-10 14:49:55 +0300

    В модели группы реализованы методы сравнения по id и name и метод строкового представления объекта Group. В тест удаления группы добавлена проверка равенства объектов из старого и нового списка по их идентификаторам и имени

--- test/test_delete_group.py
+++ test/test_delete_group.py
@@ -7,5 +7,6 @@ def test_delete_first_group(app):
     old_groups = app.group.get_groups_list()
     app.group.delete_first()
     new_groups = app.group.get_groups_list()
-
     assert len(new_groups) == len(old_groups) - 1
+    old_groups[0:1] = []
+    assert new_groups == old_groups
