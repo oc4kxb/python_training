e5e96234dba95cea6968113d8bb82928e1426885
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-03-26 18:59:45 +0300

    Реализована параметризация тестовых данных с помощью парсера getopt

--- generator/group.py
+++ generator/group.py
@@ -2,7 +2,25 @@ import json
 import random
 import string
 import os.path
+import getopt
+import sys
 from model.group import Group
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/groups.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
 
 
 def random_string(prefix, maxlen):
@@ -12,7 +30,7 @@ def random_string(prefix, maxlen):
 
 empty_and_some_random = [Group(name="", header="", footer="")] + [
     Group(name=random_string('name', 10), header=random_string('header', 20), footer=random_string('footer', 20))
-    for i in range(5)
+    for i in range(n)
 ]
 
 all_variants_with_empty = [
@@ -22,7 +40,7 @@ all_variants_with_empty = [
     for footer in ["", random_string('footer', 20)]
 ]
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../data/groups.json")
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
-with open(file, "w") as f:
-    f.write(json.dumps(empty_and_some_random, default=lambda x: x.__dict__, indent=2))
+with open(file, "w") as out:
+    out.write(json.dumps(empty_and_some_random, default=lambda x: x.__dict__, indent=2))
