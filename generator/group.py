d80cd867dadf2552b64375df1616b7352afbeac9
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-03-30 20:01:58 +0300

    Переделан генератор тестовых данных для групп с использованием jsonpickle

--- generator/group.py
+++ generator/group.py
@@ -1,4 +1,4 @@
-import json
+import jsonpickle
 import random
 import string
 import os.path
@@ -43,4 +43,5 @@ all_variants_with_empty = [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(empty_and_some_random, default=lambda x: x.__dict__, indent=2))
+    jsonpickle.set_encoder_options("json", indent=2)
+    out.write(jsonpickle.encode(empty_and_some_random))
