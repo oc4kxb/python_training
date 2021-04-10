40b822628431fd0b926324539600bddd0d2c0edf
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-04-10 03:21:04 +0300

    Добавлена связь сущностей группа и контакт по средствам связной таблицы address_in_groups. Реализован метод получения контактов из определенной группы. Проверено получение списка контактов из определенной группы в модуле check_db_connection.py

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,9 +1,10 @@
 from fixture.orm import ORMFixture
+from model.group import Group
 
 db = ORMFixture(host="localhost", database="addressbook", user="root", password="")
 
 try:
-    list = db.get_contacts_list()
+    list = db.get_contacts_in_group(Group(id=6))
     for element in list:
         print(element)
     print(len(list))
