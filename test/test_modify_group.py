02b95ed64743b614dcc18aab995484eabe07969e
Evgeni Bayrambekov <oc4kxb@gmail.com>
2021-04-10 21:52:54 +0300

    В тесты групп добавлены отключаемые проверки пользовательского интерфейса https://oc4kxb.myjetbrains.com/youtrack/issue/PT-7

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,7 +2,7 @@ import random
 from model.group import Group
 
 
-def test_modify_group_name(app, db, data_groups):
+def test_modify_group_name(app, db, check_ui, data_groups):
     if len(db.get_groups_list()) == 0:
         app.group.create(data_groups)
     old_groups = db.get_groups_list()
@@ -12,9 +12,11 @@ def test_modify_group_name(app, db, data_groups):
     assert app.group.count() == len(old_groups)
     new_groups = db.get_groups_list()
     assert sorted(new_groups, key=Group.id_or_max) == sorted(old_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_groups_list(), key=Group.id_or_max)
 
 
-def test_modify_group_header(app, db, data_groups):
+def test_modify_group_header(app, db, check_ui, data_groups):
     if len(db.get_groups_list()) == 0:
         app.group.create(data_groups)
     old_groups = db.get_groups_list()
@@ -24,9 +26,11 @@ def test_modify_group_header(app, db, data_groups):
     assert app.group.count() == len(old_groups)
     new_groups = db.get_groups_list()
     assert sorted(new_groups, key=Group.id_or_max) == sorted(old_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_groups_list(), key=Group.id_or_max)
 
 
-def test_modify_group_footer(app, db, data_groups):
+def test_modify_group_footer(app, db, check_ui, data_groups):
     if len(db.get_groups_list()) == 0:
         app.group.create(data_groups)
     old_groups = db.get_groups_list()
@@ -36,3 +40,5 @@ def test_modify_group_footer(app, db, data_groups):
     assert app.group.count() == len(old_groups)
     new_groups = db.get_groups_list()
     assert sorted(new_groups, key=Group.id_or_max) == sorted(old_groups, key=Group.id_or_max)
+    if check_ui:
+        assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_groups_list(), key=Group.id_or_max)
